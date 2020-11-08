66c56bd9d51661009a0c67668606c94a8df509d4
Walker Williams <walkerw97@gmail.com>
2020-11-08 12:30:09 -0500

    Updated Simon Says .ino to be more specific and less hard-codey.

--- sketch_simon_says.ino
+++ sketch_simon_says.ino
@@ -1,168 +1,209 @@
 // Initialize the hardware to software configuration:
-const int buttonPin1 = 2;
-const int buttonPin2 = 3;
-const int buttonPin3 = 4;
-const int buttonPin4 = 5;
+enum BUTTONPINS : uint_least8_t
+{
+    BUTTONPIN1 = 2,
+    BUTTONPIN2 = 3,
+    BUTTONPIN3 = 4,
+    BUTTONPIN4 = 5
+};
 
-const int ledPin1 =  9;
-const int ledPin2 =  10;
-const int ledPin3 =  11;
-const int ledPin4 =  12;
+enum LEDPINS : uint_least8_t
+{
+    LEDPIN1 = 9,
+    LEDPIN2 = 10,
+    LEDPIN3 = 11,
+    LEDPIN4 = 12
+};
 
-void setup() {
-  // initialize the pushbutton pins as input:
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  pinMode(buttonPin3, INPUT);
-  pinMode(buttonPin4, INPUT);
+void setup()
+{
+    // initialize the pushbutton pins as input:
+    pinMode(BUTTONPIN1, INPUT);
+    pinMode(BUTTONPIN2, INPUT);
+    pinMode(BUTTONPIN3, INPUT);
+    pinMode(BUTTONPIN4, INPUT);
 
-  // Initialize the LED pins as output:
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  pinMode(ledPin3, OUTPUT);
-  pinMode(ledPin4, OUTPUT);
+    // Initialize the LED pins as output:
+    pinMode(LEDPIN1, OUTPUT);
+    pinMode(LEDPIN2, OUTPUT);
+    pinMode(LEDPIN3, OUTPUT);
+    pinMode(LEDPIN4, OUTPUT);
 
-  // Instantiate a random number generator (Note that this has a seed based on other factors from Pin 0):
-  randomSeed(analogRead(0));
+    // Instantiate a random number generator (Note that this has a seed based on other factors from Pin 0):
+    randomSeed(analogRead(0));
 }
 
-void loop() {
-  // Setting game length (in number of turns) and array to store Simon's previous commands:
-  int gameTime = 15;
-  int gameArray[gameTime];
+void loop()
+{
+    // Setting game length (in number of turns) and array to store Simon's commands:
+    const uint_least8_t gameTime = 254; // 255 is max, but the < (this + 1) in the for loops changes values and adds bytes. This is high enough.
+    uint_least8_t gameArray[gameTime];
 
-  // Storing button states, default zero/LOW:
-  int button1 = 0;
-  int button2 = 0;
-  int button3 = 0;
-  int button4 = 0;
+    // Stores how many times we will do the "victory screech!" (victory loop) along with random number enum:
+    const uint_least8_t victoryScreech = 3;
+    enum RANDNUMS : uint_least8_t
+    {
+        RAND1,
+        RAND2,
+        RAND3,
+        RAND4
+    };
 
-  // Randomly assigns values to the game array:
-  for (int setValues = 0; setValues < gameTime; setValues++) {
-    gameArray[setValues] = random(1, 5);
-  }
-  
-  // Loop until first two buttons are pressed together then both released - Start the game:
-  while (button1 == LOW || button2 == LOW) {
-    button1 = digitalRead(buttonPin1);
-    button2 = digitalRead(buttonPin2);
-  }
-  while (button1 == HIGH || button2 == HIGH) {
-    button1 = digitalRead(buttonPin1);
-    button2 = digitalRead(buttonPin2);
-  }
+    // Stores delay values, I don't like hardcoding:
+    enum DELAYS : uint_least16_t
+    {
+        DELAY150 = 150,
+        DELAY300 = 300,
+        DELAY500 = 500,
+        DELAY750 = 750
+    };
 
-  // Loop over the game array:
-  for (int mainLoop = 0; mainLoop < gameTime; mainLoop++) {
+    // Storing button states, default LOW:
+    uint_least8_t button1 = LOW;
+    uint_least8_t button2 = LOW;
+    uint_least8_t button3 = LOW;
+    uint_least8_t button4 = LOW;
 
-    // Loop over the current game array state and display the next "order" that Simon will give:
-    for (int displayLoop = 0; displayLoop < mainLoop + 1; displayLoop++) {
-
-      // Light up the correct LED based on the number in the game array:
-      delay(150);
-      if (gameArray[displayLoop] == 1) {
-        digitalWrite(ledPin1, HIGH);
-        delay(300);
-        digitalWrite(ledPin1, LOW);
-        delay(150);
-      }
-      else if (gameArray[displayLoop] == 2) {
-        digitalWrite(ledPin2, HIGH);
-        delay(300);
-        digitalWrite(ledPin2, LOW);
-        delay(150);
-      }
-      else if (gameArray[displayLoop] == 3) {
-        digitalWrite(ledPin3, HIGH);
-        delay(300);
-        digitalWrite(ledPin3, LOW);
-        delay(150);
-      }
-      else {
-        digitalWrite(ledPin4, HIGH);
-        delay(300);
-        digitalWrite(ledPin4, LOW);
-        delay(150);
-      }
+    // Randomly assigns values to the game array:
+    for (uint_least8_t setValues = 0; setValues < gameTime; ++setValues)
+    {
+        gameArray[setValues] = random(RAND1, RAND4 + 1);
     }
 
-    // For each "order" Simon gives you have to repeat it, this is where that happens:
-    for (int inputLoop = 0; inputLoop < mainLoop + 1; inputLoop++) {
-
-      // Accept the first input the user gives, wait otherwise:
-      while (button1 == LOW && button2 == LOW && button3 == LOW && button4 == LOW) {
-        button1 = digitalRead(buttonPin1);
-        button2 = digitalRead(buttonPin2);
-        button3 = digitalRead(buttonPin3);
-        button4 = digitalRead(buttonPin4);
-        delay(100);
-      }
-
-      // check which button was pressed then if that was the correct "order":
-      if (button1 == HIGH && gameArray[inputLoop] == 1) {
-        digitalWrite(ledPin1, HIGH);
-        while (button1 == HIGH) {
-          button1 = digitalRead(buttonPin1);
-        }
-        delay(150);
-        digitalWrite(ledPin1, LOW);
-      }
-      else if (button2 == HIGH && gameArray[inputLoop] == 2) {
-        digitalWrite(ledPin2, HIGH);
-        while (button2 == HIGH) {
-          button2  = digitalRead(buttonPin2);
-        }
-        delay(150);
-        digitalWrite(ledPin2, LOW);
-      }
-      else if (button3 == HIGH && gameArray[inputLoop] == 3) {
-        digitalWrite(ledPin3, HIGH);
-        while (button3 == HIGH) {
-          button3 = digitalRead(buttonPin3);
-        }
-        delay(150);
-        digitalWrite(ledPin3, LOW);
-      }
-      else if (button4 == HIGH && gameArray[inputLoop] == 4) {
-        digitalWrite(ledPin4, HIGH);
-        while (button4 == HIGH) {
-          button4 = digitalRead(buttonPin4);
-        }
-        delay(150);
-        digitalWrite(ledPin4, LOW);
-      }
-      // If they got the "order" wrong, the game is over:
-      else {
-        digitalWrite(ledPin1, HIGH);
-        digitalWrite(ledPin2, HIGH);
-        digitalWrite(ledPin3, HIGH);
-        digitalWrite(ledPin4, HIGH);
-        delay(750);
-        digitalWrite(ledPin1, LOW);
-        digitalWrite(ledPin2, LOW);
-        digitalWrite(ledPin3, LOW);
-        digitalWrite(ledPin4, LOW);
-        delay(500);
-        return;
-      }
-
-      // If all the "orders" were correct, and there are none left, the user has won the game:
-      if (inputLoop == gameTime - 1) {
-        delay(150);
-        for (int victoryLoop = 0; victoryLoop < 3; victoryLoop++) {
-          digitalWrite(ledPin1, HIGH);
-          digitalWrite(ledPin2, HIGH);
-          digitalWrite(ledPin3, HIGH);
-          digitalWrite(ledPin4, HIGH);
-          delay(500);
-          digitalWrite(ledPin1, LOW);
-          digitalWrite(ledPin2, LOW);
-          digitalWrite(ledPin3, LOW);
-          digitalWrite(ledPin4, LOW);
-          delay(500);
-        }
-        return;
-      }
+    // Loop until first two buttons are pressed together then both released - Start the game:
+    while (button1 == LOW || button2 == LOW)
+    {
+        button1 = digitalRead(BUTTONPIN1);
+        button2 = digitalRead(BUTTONPIN2);
     }
-  }
+    while (button1 == HIGH || button2 == HIGH)
+    {
+        button1 = digitalRead(BUTTONPIN1);
+        button2 = digitalRead(BUTTONPIN2);
+    }
+
+    // Loop over the game array:
+    for (uint_least8_t mainLoop = 0; mainLoop < gameTime; ++mainLoop)
+    {
+        // Loop over the current game array state and display the next "order" that Simon will give:
+        for (uint_least8_t displayLoop = 0; displayLoop < (mainLoop + 1); ++displayLoop)
+        {
+            // Light up the correct LED based on the number in the game array:
+            delay(DELAY300);
+            if (gameArray[displayLoop] == RAND1)
+            {
+                digitalWrite(LEDPIN1, HIGH);
+                delay(DELAY300);
+                digitalWrite(LEDPIN1, LOW);
+            }
+            else if (gameArray[displayLoop] == RAND2)
+            {
+                digitalWrite(LEDPIN2, HIGH);
+                delay(DELAY300);
+                digitalWrite(LEDPIN2, LOW);
+            }
+            else if (gameArray[displayLoop] == RAND3)
+            {
+                digitalWrite(LEDPIN3, HIGH);
+                delay(DELAY300);
+                digitalWrite(LEDPIN3, LOW);
+            }
+            else
+            {
+                digitalWrite(LEDPIN4, HIGH);
+                delay(DELAY300);
+                digitalWrite(LEDPIN4, LOW);
+            }
+        }
+
+        // For each "order" Simon gives, you have to repeat it, this is where that happens:
+        for (uint_least8_t inputLoop = 0; inputLoop < (mainLoop + 1); ++inputLoop)
+        {
+            // Accept the first input the user gives, wait otherwise:
+            while (button1 == LOW && button2 == LOW && button3 == LOW && button4 == LOW)
+            {
+                button1 = digitalRead(BUTTONPIN1);
+                button2 = digitalRead(BUTTONPIN2);
+                button3 = digitalRead(BUTTONPIN3);
+                button4 = digitalRead(BUTTONPIN4);
+                delay(DELAY150);
+            }
+
+            // check which button was pressed, then if that was the correct "order" light up that led:
+            if (button1 == HIGH && gameArray[inputLoop] == RAND1)
+            {
+                digitalWrite(LEDPIN1, HIGH);
+                while (button1 == HIGH)
+                {
+                    button1 = digitalRead(BUTTONPIN1);
+                }
+                delay(DELAY150);
+                digitalWrite(LEDPIN1, LOW);
+            }
+            else if (button2 == HIGH && gameArray[inputLoop] == RAND2)
+            {
+                digitalWrite(LEDPIN2, HIGH);
+                while (button2 == HIGH)
+                {
+                    button2 = digitalRead(BUTTONPIN2);
+                }
+                delay(DELAY150);
+                digitalWrite(LEDPIN2, LOW);
+            }
+            else if (button3 == HIGH && gameArray[inputLoop] == RAND3)
+            {
+                digitalWrite(LEDPIN3, HIGH);
+                while (button3 == HIGH)
+                {
+                    button3 = digitalRead(BUTTONPIN3);
+                }
+                delay(DELAY150);
+                digitalWrite(LEDPIN3, LOW);
+            }
+            else if (button4 == HIGH && gameArray[inputLoop] == RAND4)
+            {
+                digitalWrite(LEDPIN4, HIGH);
+                while (button4 == HIGH)
+                {
+                    button4 = digitalRead(BUTTONPIN4);
+                }
+                delay(DELAY150);
+                digitalWrite(LEDPIN4, LOW);
+            }
+            // If they got the "order" wrong, the game is over:
+            else
+            {
+                digitalWrite(LEDPIN1, HIGH);
+                digitalWrite(LEDPIN2, HIGH);
+                digitalWrite(LEDPIN3, HIGH);
+                digitalWrite(LEDPIN4, HIGH);
+                delay(DELAY750);
+                digitalWrite(LEDPIN1, LOW);
+                digitalWrite(LEDPIN2, LOW);
+                digitalWrite(LEDPIN3, LOW);
+                digitalWrite(LEDPIN4, LOW);
+                return;
+            }
+
+            // If all the "orders" were correct, and there are none left, the user has won the game:
+            if (inputLoop == (gameTime - 1))
+            {
+                delay(DELAY150);
+                for (uint_least8_t victoryLoop = 0; victoryLoop < victoryScreech; ++victoryLoop)
+                {
+                    digitalWrite(LEDPIN1, HIGH);
+                    digitalWrite(LEDPIN2, HIGH);
+                    digitalWrite(LEDPIN3, HIGH);
+                    digitalWrite(LEDPIN4, HIGH);
+                    delay(DELAY500);
+                    digitalWrite(LEDPIN1, LOW);
+                    digitalWrite(LEDPIN2, LOW);
+                    digitalWrite(LEDPIN3, LOW);
+                    digitalWrite(LEDPIN4, LOW);
+                    delay(DELAY500);
+                }
+                return;
+            }
+        }
+    }
 }
